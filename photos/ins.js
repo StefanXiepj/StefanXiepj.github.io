513b0429a710a33d5d8ad5c57059abf649152d0f
xiepengjie <xiepengjie@didiglobal.com>
2021-04-25 23:55:18 +0800

    Site updated: 2021-04-25 23:55:17

--- photos/ins.js
+++ photos/ins.js
@@ -119,8 +119,8 @@
                 for (var i = 0, len = data.link.length; i < len; i++) {
                     /*var minSrc = 'https://raw.githubusercontent.com/ainimini/Picture/master/' + data.link[i];
                     var src = 'https://raw.githubusercontent.com/ainimini/Picture/master/' + data.link[i];*/
-                    var minSrc = 'https://github.com/StefanXiepj/BlogPicture/raw/main/min_photos/' + data.link[i];
-                    var src = 'https://github.com/StefanXiepj/BlogPicture/raw/main/photos/' + data.link[i];
+                    var minSrc = 'https://raw.githubusercontent.com/StefanXiepj/BlogPicture/raw/main/min_photos/' + data.link[i];
+                    var src = 'https://raw.githubusercontent.com/StefanXiepj/BlogPicture/raw/main/photos/' + data.link[i];
                     var type = data.type[i];
                     var target = src + (type === 'video' ? '.mp4' : '.jpg');
                     src += '';
